abb07f9dfd20f63db0f9c51232eb276bce98d99d
SciarriniAlessandroITT <172112950+SciarriniAlessandro@users.noreply.github.com>
2026-04-14 12:31:39 +0200

    feat(ui): add search and detail pages with mock data
    - Implemented Search view with prenotations list and filtering UI (class, room, hours)
    - Implemented Detail view to display prenotation information (classes, room, date, time, teacher)
    
    - Added temporary mock data for development (pending backend integration)
    
    - Minor UI adjustments:
      - Updated header styling
      - Adjusted logo dimensions to match header layout
      - Updated theme component styling for standalone usage (without header)
    
    Note: backend integration pending

--- Frontend/src/app/features/bookings/detail/detail.ts
+++ Frontend/src/app/features/bookings/detail/detail.ts
@@ -6,7 +6,16 @@ import { HeaderComponent } from '../../../shared/components/header/header';
 @Component({
     selector: 'app-detail',
     templateUrl: './detail.html',
-    // styleUrl: './detail.css',
+    styleUrl: './detail.css',
     imports: [Page, HeaderComponent],
 })
-export class Detail {}
+export class Detail {
+    prenotation = {
+        classes: [ {id: 0, c: '5BIA'}, {id: 1, c: '5CIA'}, {id: 2, c: '5DIA'} ],
+        room: 119,
+        date: '17/04/2026',
+        start: '08:00',
+        end: '10:00',
+        teacher: 'Ferraro Patrizia'
+    }
+}
